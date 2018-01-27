01990dd68f552993047725e1bf9e7be5af4b4769
Armen Paronikyan <armparonikyan@gmail.com>
2018-01-27 17:32:26 +0400

    Change ABI

--- FuturesExample.sol
+++ FuturesExample.sol
@@ -1,5 +1,5 @@
 contract BinaryFuture{
-    
+
     struct Future{
         uint256 expires;
         uint256 buyIn;
@@ -10,9 +10,9 @@ contract BinaryFuture{
         address long;
         address short;
         address winner;
-        FuturesStage stage;        
+        FuturesStage stage;
     }
-    
+
     enum FuturesStage{
         Unfilled,
         ReadyStart,
@@ -20,22 +20,22 @@ contract BinaryFuture{
         ReadyEnd,
         ReadySettle
     }
-    
+
     enum Positions{
         Long,
         Short
     }
-    
+
     event Query(string queryString, address queryAddress);
-    
+
     Future future;
-    
+
     function BinaryFuture(
-        uint256 expires, 
-        uint256 buyIn, 
-        string queryString, 
+        uint256 expires,
+        uint256 buyIn,
+        string queryString,
         address queryAddress){
-            
+
         future.expires = expires;
         future.buyIn = buyIn;
         future.stage = FuturesStage.Unfilled;
@@ -47,49 +47,49 @@ contract BinaryFuture{
     function fillPosition(uint256 position) public payable {
 
     }
-    
+
     //make query for future.startValue
     function startContract() public {
 
     }
-    
+
     //make query for future.endValue
     function endContract() public {
-        
+
     }
-    
+
     //compare future.startValue with future.endValue, payout to and short proportionally
     function settleContract() public {
-    
+
     }
-    
+
     //emit query event
     function queryOracle(string stage, string query) internal {
-     
+        Query("armen", msg.sender);
     }
-    
+
     //handle oracle response
     function oracleCallback(uint256 response){
-        
+
     }
-        
-    //are positions unfilled =>Unfilled (0)    
+
+    //are positions unfilled =>Unfilled (0)
     //is contract ready to start ( make query for future.startValue) =>ReadyStart (1)
     //is contract startValue set and is yet to expire =>ContractLive (2)
     //is contract ready to end (make query for future.endValue) =>ReadyEnd (3)
     //is contract ready to settle payouts via future.startValue and future.endValue =>ReadySettled (4)
     function getStatus() constant returns(FuturesStage stage){
-        
-    }        
-        
+
+    }
+
     function getBuyIn() constant returns(uint256 buyIn){
         return future.buyIn;
-    }   
-        
+    }
+
     function getWinner() constant returns(address winner){
         return future.winner;
     }
-    
+
     function getLong() constant returns(address long){
         return future.long;
     }
@@ -97,5 +97,5 @@ contract BinaryFuture{
     function getShort() constant returns(address short){
         return future.short;
     }
-    
+
 }
